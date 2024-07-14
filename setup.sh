88fe3b43e1b77f9ec35d4a1fdd096996a15dbdab
sjoshuam <sjoshuam@github.com>
2024-07-14 11:47:42 -0400

    Expand set up file to include git initialization

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
-## run at the start of each session
+## RUN AT THE START OF EACH SESSION
+
+## navigate to the right directory and activate virtual environment
 cd ~/code/color_math
 source ~/env/color_math/bin/activate
 
-## run initially to set up virtual environment
+## RUN INITIALLY TO SET UP THE ENVIRONMENT
+
+## set up a virtual environment with the right Python packages
 mkdir ~/code/color_math
 cd ~/code/color_math
 python3.12 -m venv ~/env/color_math
@@ -10,10 +14,14 @@ source ~/env/color_math/bin/activate
 pip install --upgrade pip
 pip install pandas==2.2.2 plotly==5.22.0 openpyxl==3.1.5
 
+## configure git to ignore caches and invisible
 echo ".*
 io_mid/*
 __pycache__" > .gitignore
 
+## commit directory and upload to github
 git init
 git add -A
-git 
+git commit -m "Upload initial files"
+git remote add origin git@github.com:sjoshuam/color_math.git
+git branch -M main
